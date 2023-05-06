e2e9ae529f02ee2e30321ba9063514a971966977
Aldrin Ricardo <aldrinricardo96@gmail.com>
2023-05-06 15:01:09 -0300

    Added input exceptions

--- src/ticTacToe/Program.java
+++ src/ticTacToe/Program.java
@@ -1,11 +1,12 @@
 package ticTacToe;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
 		Match match = new Match();
 		int turns = 0;
@@ -16,25 +17,30 @@ public class Program {
 		System.out.println();
 		match.printPositions();
 		match.printBoard();
-		while (winner == null){
-			if (turns == 9) {
-				System.out.println("Draw!");
-				break;
-			}
-			System.out.println("Turn: " + turn);
-			System.out.print("Choose the position you want: ");
-			int chosenPosition = sc.nextInt();
-			match.placeAndPrint(chosenPosition, turn);
-			winner = match.checkWinner();
-			if (turn == 'X') {
-				turn = 'O';
-			} else {
-				turn = 'X';
-			}		
-			turns++;
-			
-		}	System.out.printf("%s won!", winner, turns); 
-		
+		try {
+			while (winner == null){
+				if (turns == 9) {
+					System.out.println("Draw!");
+					break;
+				}
+				System.out.println("Turn: " + turn);
+				System.out.print("Choose the position you want (0-8): ");
+				int chosenPosition = sc.nextInt();
+				match.placeAndPrint(chosenPosition, turn);
+				winner = match.checkWinner();
+				if (turn == 'X') {
+					turn = 'O';
+				} else {
+					turn = 'X';
+				}		
+				turns++;
+				
+			}	System.out.printf("Winner: %s ", winner); 
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid position! Chosse a number between 0-8 next time");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Position out of bounds! Choose a position between 0-8 next time");
+		}
 		sc.close();
 	}
 }
